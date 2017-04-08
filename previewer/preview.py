7dc8a8b9f228c06f3c24fe8f73199d79601f98ee
Baptiste Fontaine <b@ptistefontaine.fr>
2017-04-08 22:08:43 +0200

    Remove a debugging print

--- previewer/preview.py
+++ previewer/preview.py
@@ -58,7 +58,6 @@ class Preview:
         text = fit_text(text, font, self._max_width,
                 max_height, max_lines=max_lines)
 
-        print(text)
         _, h = self._draw.textsize(text)
 
         self._draw.text((self._padding_x, self._y),
